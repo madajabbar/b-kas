df5f7b263443ae3bd762cbb8417981b253ffe049
madajabbar <madajabbar22@gmail.com>
2023-09-24 23:24:42 +0700

    fix

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -1,29 +1,4 @@
 <footer class="footer_dark">
-    <div class="footer_top">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-4 col-md-6 col-sm-12">
-                    <div class="widget">
-                        <h6 class="widget_title">Contact info</h6>
-                        <ul class="contact_info contact_info_light">
-                            <li>
-                                <i class="ti-location-pin"></i>
-                                <p>Pontianak, Indonesia</p>
-                            </li>
-                            <li>
-                                <i class="ti-email"></i>
-                                <a href="mailto:bkas@gmail.com">bkas@gmail.com</a>
-                            </li>
-                            <li>
-                                <i class="ti-mobile"></i>
-                                <p>+ 62 </p>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
     <div class="bottom_footer border-top-tran">
         <div class="container">
             <div class="row">
